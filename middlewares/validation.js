9fa9c5be95131c42bdc5f9cc338f5a60719a3e8c
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-08-08 13:56:50 +0300

    +отредактировал валидацию селебрейт

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -30,14 +30,21 @@ const validateUpdateProfile = celebrate({
       .required(),
   }),
 });
-// Валидация карточек
-// Создание карточки
+// Валидация фильма
+// Создание фильма
 const validateCreateMovie = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30)
-      .required(),
-    link: Joi.string().pattern(urlRegex)
-      .required(),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(urlRegex),
+    trailerLink: Joi.string().required().pattern(urlRegex),
+    thumbnail: Joi.string().required().pattern(urlRegex),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
   }),
 });
 // Валидация id карточки
