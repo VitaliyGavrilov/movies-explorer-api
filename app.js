3189ac6546b9e5c5cdc5778d6671781a7de15c0e
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-08-10 15:31:39 +0300

    +helmet

--- app.js
+++ app.js
@@ -5,29 +5,26 @@ const express = require('express');// сервер
 const mongoose = require('mongoose');// бд
 const { errors } = require('celebrate');
 const cors = require('cors');
+const helmet = require('helmet');
 const { requestLogger, errorLogger } = require('./middlewares/logger');// импорт логеров
 // задаем переменные окружения
 const { PORT = 3000, BASE_PATH = 'http://localhost:3000', MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;// переменные окружения
 // запускаем сервер
 const app = express();
-// для корс ошибок
-app.use(cors());
-// подключаемся к бд
-mongoose.connect(MONGO_URL)
+// подключаем
+app.use(cors()); // для корс ошибок
+app.use(helmet()); // для установки заголовков, связанных с безопасностью.
+mongoose.connect(MONGO_URL) // подключаемся к бд
   .then(() => console.log('Мы подлюченны к MongoDB'))
   .catch((err) => console.log(`Мы не подлюченны к MongoDB, ошибка: ${err}`));
-// типо говорим с каким форматом данных будет работать наш сервер
 app.use(express.json()); // анализирует входящие запросы JSON и помещает данные в req.body.
-// подключаем логгер запросов
-app.use(requestLogger);
-// испоьзуем биг-роут
-app.use('/', require('./routes/router'));
-// подключаем логгер ошибок
-app.use(errorLogger);
+app.use(requestLogger); // подключаем логгер запросов
+// роуты
+app.use('/', require('./routes/router')); // испоьзуем биг-роут
 // обработчики ошибок
+app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
-// подключаем централизованный обработчик ошибок
-app.use(require('./middlewares/errorHandling'));
+app.use(require('./middlewares/errorHandling')); // подключаем централизованный обработчик ошибок
 // слушатель
 app.listen(PORT, () => {
   console.log(`Сервер по адресу ${BASE_PATH} отлично работает, порт: ${PORT}`);
